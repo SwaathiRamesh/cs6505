85ce625e9a082ebd2730ca40f70a2629e84ecca0
swat <swramesh92@gmail.com>
2015-08-22 14:45:46 -0400

    Finished program

--- normalmatrixmultiply.py
+++ normalmatrixmultiply.py
@@ -3,9 +3,18 @@ B=[[0 for x in range(5)] for x in range(5)]
 for i in range(5):
     for j in range(5):
         A[i][j]=2*i+j
-        B[i][j]=2*j-i
+        B[j][i]=j-2*i
 
 print "Matrix A is"
 print A
 print "Matrix B is"
 print B
+
+Result=[[0 for x in range(5)] for x in range(5)]
+
+for i in range(5):
+    for j in range(5):
+        Result[i][j]=0
+        for k in range(5):
+            Result[i][j]+=A[i][k]+B[k][j]
+print Result
